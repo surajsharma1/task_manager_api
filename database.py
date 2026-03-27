da0fc7a9a589b2430738d95b6d82ee1efdbeac46
surajsharma1 <surajsharma424255@gmail.com>
2026-03-27 16:45:50 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,23 +1,28 @@
-import sqlite3
+import psycopg2
+import os
+from dotenv import load_dotenv
 
-conn = sqlite3.connect("tasks.db")
+load_dotenv()
+
+conn = psycopg2.connect(os.getenv("DATABASE_URL"))
 cursor = conn.cursor()
 
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         username TEXT NOT NULL,
         password TEXT NOT NULL
     )
 """)
 
 cursor.execute("""
-    CREATE TABLE IF NOT EXISTS tasks(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS tasks (
+        id SERIAL PRIMARY KEY,
         title TEXT NOT NULL,
         username TEXT NOT NULL,
         status INTEGER NOT NULL DEFAULT 0
     )
 """)
 
+conn.commit()
 conn.close()
